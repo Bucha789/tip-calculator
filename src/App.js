e8c641ab181f4c5c43d86c8fb798510ac15383c9
bucha789 <cursosdetuvida100@gmail.com>
2021-07-30 18:37:25 -0500

    añadiendo los estilos globales

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import ResultsContainer from './components/ResultsContainer';
 function App() {
   return (
     <div className="App">
-      <h1>Splitter</h1>
+      <h1>Spli<br></br>tter</h1>
       <div className="grid__container">
       <InputContainer />
       <ResultsContainer/>
